e48c44f6742c13ea6a730742f45fca2942eab0ff
sharonsyra <sharonwaithii@gmail.com>
2025-03-27 13:46:47 +0300

    Project: Dbt Clean up

--- dbt/beverage_sales/models/core/fact_sales.sql
+++ dbt/beverage_sales/models/core/fact_sales.sql
@@ -1,9 +1,35 @@
 {{
     config(
-        materialized='table'
+        materialized='view'
     )
 }}
 
+with salesdata as
+(
+  select *,
+    row_number() over(partition by Order_ID, Customer_ID) as rn
+  from {{ source('core','synthetic_beverage_sales_data') }}
+  where Order_ID is not null
+)
 select
-    *
-from {{ ref('stg_synthetic_beverage_sales_data') }}
+    {{ dbt_utils.generate_surrogate_key(['Order_ID', 'Customer_ID']) }} as Sale_ID,
+    Order_ID,
+    Customer_ID,
+    Customer_Type,
+    Product,
+    Category,
+    Unit_Price,
+    Quantity,
+    Discount,
+    Total_Price,
+    Region,
+    Order_Date
+from salesdata
+where rn = 1
+
+-- dbt build --select <model_name> --vars '{'is_test_run': 'false'}'
+{% if var('is_test_run', default=true) %}
+
+  limit 100
+
+{% endif %}
